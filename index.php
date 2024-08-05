560676ff741a8d217cf840bd132047c02bcd4fc9
KhanPromiseP <khanpennpromise@gmail.com>
2024-08-05 17:35:56 +0100

    Hello World in PHP

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+    <linnk rel="stylesheet" href=" style.css">
+</head>
+
+<body>
+    <div class="container">
+        <?php echo"Hello World!"; ?>
+    </div>
+</body>
+
+</html>
